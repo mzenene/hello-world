4d4bb3e80e821391d62ae8831ca8a532608986ab
Mzenene <ntsikelelondleleni@gmail.com>
2023-12-10 20:21:41 +0200

    actually classes are added in this commit

---
+++ classes.py
@@ -0,0 +1,8 @@
+class Umoliso():
+    def __init__(self,igama,iminyaka,ilizwe):
+        self.igama=igama
+        self.iminyaka=iminyaka
+        self.ilizwe=ilizwe
+    def veza(igama,iminyaka,ilizwe):
+        if(igama!=None):
+            print(f"Molo {igama} uneminyaka e{iminyaka} uvela kwilizwe lase {ilizwe}")
