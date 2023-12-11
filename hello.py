a3cf2f4debee60cea3158a478246f7fcad0672e6
Mzenene <ntsikelelondleleni@gmail.com>
2023-12-11 05:33:53 +0200

    added my prompts in function, that returns an object of my class

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 import classes
 
-igama = input("Ungubani?: ")
-iminyaka =input("Uneminyaka emingaphi?: ")
-ilizwe =input("uvela phi?: ")
+def buza():
+     igama = input("Ungubani?: ")
+     iminyaka =input("Uneminyaka emingaphi?: ")
+     ilizwe =input("uvela phi?: ")
 
-classes.Umbhuliso.veza(igama,iminyaka,ilizwe)
+     umbhuliso =classes.Umbhuliso(igama,iminyaka,ilizwe)
+     return umbhuliso
+    
+umbhuliso=buza()
+classes.Umbhuliso.veza(umbhuliso.igama,umbhuliso.iminyaka,umbhuliso.ilizwe)
