da39121eca808d0150c87e23cb4ee689ba95fb0c
Mzenene <ntsikelelondleleni@gmail.com>
2023-12-11 06:01:03 +0200

    checked for empty igama

--- classes.py
+++ classes.py
@@ -5,5 +5,7 @@ class Umbhuliso():
         self.ilizwe=ilizwe
 
     def veza(igama,iminyaka,ilizwe):
-        if(igama!=None):
+        if(igama!=""):
             print(f"Molo {igama} uneminyaka eyi{iminyaka} uvela kwilizwe lase {ilizwe}")
+        else:
+            print(f"Yazi uwuva ! ndithe kuwe ubhale igama,iminyaka nendawo uvela kuyo")
